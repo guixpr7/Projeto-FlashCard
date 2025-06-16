220dcfc70477ed680d776a3e79138112cca906fa
guixpr7 <guilherme.martins.santos15@escola.pr.gov.br>
2025-06-16 11:03:50 +0000

    alterações

---
+++ arquivosJS/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Noruega?',
+    'Capital é Oslo.'
+)
+
+criaCartao(
+    'História',
+    'Quem escreveu Dom Quixote?',
+    'Escritor chamado Miguel de Cervantes.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do planeta?',
+    'O Oceano Pacífico.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os países que fazem fronteira com o Brasil?',
+    'Os países são: Guiana Francesa, Suriname, Guiana, Venezuela, Colômbia, Peru, Bolívia, Paraguai, Argentina, Uruguai.'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a diferença entre massa e peso?',
+    'A diferenaça é O peso é a força com que um corpo é atraído pela Terra. Massa é uma medida direta da oposição que um corpo oferece à mudança em seu estado de movimento.'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Quais são os principais elementos de uma narrativa literária?',
+    'são: Intodução, Desenvolvimento, Clímax e Conclusão.'
+)
